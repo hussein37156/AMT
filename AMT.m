f1ca0b34901fa939e04b56f46f3087fcddadce63
Hussein <hussein37156@gmail.com>
2025-05-05 21:06:25 +0300

    reduce model noise when shifting adjust the hystriesis

--- AMT.m
+++ AMT.m
@@ -4,7 +4,7 @@ function [i, shifting_flag,dq_v_shifting] = AMT(i, igs, dq_v, map, shifting_flag
         possiple_T_e = arrayfun(@(rpm) search(map, rpm, throtel), possiple_dq_e_RPM);
         possiple_T_o = possiple_T_e .* igs;
         [max_T, idx] = max(possiple_T_o);
-        torque_threshold = 5; 
+        torque_threshold = 1; 
         current_T = possiple_T_o(i);
         if (max_T > current_T + (torque_threshold)) && (idx > i)
             % Upshift
@@ -13,7 +13,7 @@ function [i, shifting_flag,dq_v_shifting] = AMT(i, igs, dq_v, map, shifting_flag
             dq_v_shifting=dq_v;
         elseif (max_T > current_T + (torque_threshold)) && (idx < i)
             % Downshift
-            if abs(dq_v_shifting-dq_v)>7
+            if abs(dq_v_shifting-dq_v)>21
                 i = max(i - 1, 1);
                 shifting_flag = 2;
                 dq_v_shifting=dq_v;
